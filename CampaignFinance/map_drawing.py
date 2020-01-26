f488fd795342b0c959931ee04e79098553542233
Kayvis <quantkeyvis@gmail.com>
2020-01-26 16:53:41 -0500

    Created file for drawing district maps
    first

---
+++ CampaignFinance/map_drawing.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+import numpy as np 
+import os
+import sys
+import folium
+import json
+import requests
+import geopandas 
+import hvplot
+import vincent
+
+
+m = folium.Map(location=[39.381266,-97.922211], zoom_start=5.3)
+sts="/districts/states/"
+for subdir, dirs, files in os.walk(sts):
+	if len(dirs)>3:
+		for state in dirs:
+			if len(state)<3:
+				folium.GeoJson(sts+state+"/shape.geojson", name="geojson "+state).add_to(m)
+				print(state)
+				for subdir, dirs, files in os.walk(sts+state+"/sldu/"):
+					#print(files)
+					for file in files:
+						folium.GeoJson(sts+state+"/sldu/"+file, name="geojson "+state+"-"+file.split('.')[0]).add_to(m)
+
+m.save(outfile='datamap.html')
